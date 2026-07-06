b4003f3f112b62fbb78e59cc416d18c33e90911a
faisaldp123 <faisal.dpathshala@gmail.com>
2026-07-06 18:25:52 +0530

    image issue and other detail issue is fixed

--- controllers/trackingController.js
+++ controllers/trackingController.js
@@ -19,7 +19,11 @@ export const trackOrder = async (req, res) => {
   try {
     const { id } = req.params;
 
-    const order = await Order.findById(id);
+    const order = await Order.findById(id)
+  .populate(
+    "items.product",
+    "name price images sizes"
+  );
 
     if (!order) {
       return res.status(404).json({
